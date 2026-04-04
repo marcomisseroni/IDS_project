221169cc11b9b6d5a1c8974a976fffc094e9d0d8
Fobi-02 <federico@Portatile-Federico.localdomain>
2026-04-04 18:38:34 +0200

    adapted the code for RGBD cameras

--- python_sim/target_detection.py
+++ python_sim/target_detection.py
@@ -9,15 +9,24 @@ import os
 
 # Load YOLOv8 model
 model = YOLO("yolov8n.pt")
+# camera settings
+fps = 25
+f = 50 # focal length (mm)
+rx = 1920 #resolution of the camera along x
+d = 36/rx # physical size of a pixel (mm)
+n_d = 255 # number of depth channels
 
-
-def detect_objects(frame):
+def detect_human(frame):
     results = model(frame, verbose=False)
     detected_objects = []
 
     # we want the closest (biggest) person position
     x_target = None
     y_target = None
+    x_1 = None
+    x_2 = None
+    y_1 = None
+    y_2 = None
     biggest_area = 0
     for r in results:
         for box in r.boxes:
@@ -42,29 +51,105 @@ def detect_objects(frame):
                     biggest_area = area
                     x_target = x_c
                     y_target = y_c
-                    cv2.circle(frame, (x_c, y_c), 3, (0,0,255), 3)
+                    x_1 = x1
+                    x_2 = x2
+                    y_1 = y1
+                    y_2 = y2
+    if x_target != None:
+        cv2.rectangle(frame, (x_1, y_1), (x_2, y_2), (0, 0, 255), 2)
+        cv2.circle(frame, (x_target, y_target), 3, (0,0,255), 3)
+    return frame, x_1, x_2, y_1, y_2, x_target
 
-    return frame, x_target, y_target
-
-def target_estimation(x_target_L, x_target_R):
-    f = 50 # focal length (mm)
+def target_estimation_stereo(x_target_L, x_target_R):
     T = 100 # sensor distance (mm)
     n1 = x_target_L # number of pixels from the sensor border
     n2 = x_target_R # number of pixels from the sensor border
-    d = 36/1920 # physical size of a pixel (mm)
-    rx = 1920 #resolution of the camera along x
 
     # relative position of the target in respect to the center of the two cameras (mm)
     x_relative = f*T/(d*(n1-n2))
     y_relative = -(n1+n2-rx)*T/(2*(n1-n2))
     return x_relative, y_relative
 
+def target_estimation_RGBD(x1, x2, y1, y2, xc, depth):
+    # angle in respect to the camera
+    tan_theta = 1/f*(rx/2-xc)
+    # distance from the depth image
+    # using max value of the region
+    region = depth[y1:y2, x1:x2]
+    depth_val = np.max(region)
+    D = -2970/n_d*depth_val + 3000
+
+    # distances
+    x_relative = D
+    y_relative = D*tan_theta
+    return x_relative, y_relative
+
+def main():
+    # Open the RGBD video
+    cap = cv2.VideoCapture("RGB 3.mp4")
+    cap_d = cv2.VideoCapture("Depth.mp4")
+
+    delay = int(1000 / fps)
+    
+    # vector to contain all the estimated positions
+    x_rel = []
+    y_rel = []
+
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.set_title("Target trajectory")
+    ax.set_xlabel('x [mm]')
+    ax.set_ylabel('y [mm]')
+    ax.grid(True)
+    line, = ax.plot([], [], 'b.')
+
+    while True:
+        # loading the current frame
+        ret, frame = cap.read()
+        ret_d, frame_d = cap_d.read()
+        if not ret:
+            print("ERROR IN CAPTURING")
+            break
+
+        # using YOLO to detect the target in the frame
+        frame, x1, x2, y1, y2, xc = detect_human(frame)
+
+        combined = np.hstack((frame, frame_d))
+        combined_resized = cv2.resize(combined, (1280, 360))
+        cv2.imshow("Person recognition", combined_resized)
+
+        # estimated position of the target (if found)
+        if xc != None:
+            x_relative, y_relative = target_estimation_RGBD(x1, x2, y1, y2, xc, frame_d)
+            x_rel.append(x_relative)
+            y_rel.append(y_relative)
+
+        line.set_xdata(x_rel)
+        line.set_ydata(y_rel)
+        ax.relim()
+        ax.autoscale_view()
+        plt.draw()
+        plt.pause(0.001)
+
+        if cv2.waitKey(delay) == ord('q') or not ret:
+            break
+
+    cap.release()
+    cap_d.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# CODE FOR STEREO CAMERAS
+'''
 def main():
     # Open the two stereo camera videos
     cap_L = cv2.VideoCapture("Left camera.mp4")
     cap_R = cv2.VideoCapture("Right camera.mp4")
 
-    fps = 30
     delay = int(1000 / fps)
     # filter smoothness
     alpha = 0.1
@@ -123,7 +208,4 @@ def main():
     plt.grid(True)
     plt.legend()
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
+    '''
