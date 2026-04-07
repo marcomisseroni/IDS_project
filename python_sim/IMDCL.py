50ceb0acefb4b4b85aa93ddd7141430c080c1062
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-04-07 21:03:28 +0200

    Fixed update function

--- python_sim/IMDCL.py
+++ python_sim/IMDCL.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import sqrtm
 import random
 
 class IMDCL:
@@ -20,6 +21,8 @@ class IMDCL:
         self.mu = mu
         self.sigma = sigma
         self.H = np.eye(2, 2)  # to check
+        IMDCL.agents_number += 1
+        self.id = IMDCL.agents_number
 
     def prediction(self):
         epsilon = random.gauss(self.mu, self.sigma)
@@ -27,17 +30,59 @@ class IMDCL:
         self.P = self.F @ self.P @ self.F.transpose()
         self.phi = self.F @ self.phi
 
-    def rel_meas(self, state_b, phi_b, P_b, z_ab, H_b):
-        pi_ab = self.pi12
+    def rel_meas(self, state_b, phi_b, P_b, z_ab, H_b, id_b):
+        id_a = self.id
+        pi_ab = None
+        if((id_a == 1 and id_b == 2) or (id_a == 2 and id_b == 1)):
+            pi_ab = self.pi12
+        elif((id_a == 2 and id_b == 3) or (id_a == 3 and id_b == 2)):
+            pi_ab = self.pi23
+        elif((id_a == 1 and id_b == 3) or (id_a == 3 and id_b == 1)):
+            pi_ab = self.pi13
+        if(pi_ab is None):
+            print("pi_ab not assigned")
+            return
         Pab = self.phi @ pi_ab @ phi_b.transpose()
         Pba = phi_b @ pi_ab @ self.phi.transpose()
         r_a = z_ab - (state_b - self.state)
         S_ab = self.R + self.H @ self.P @ self.H.transpose() + H_b @ P_b @ H_b.transpose() - self.H @ Pab @ H_b.transpose() - H_b @ Pba @ self.H.transpose()
-        gamma_a = (pi_ab @ phi_b.transpose() @ H_b.transpose() - self.phi.inverse() @ self.P @ self.H.transpose()) @ S_ab **-0.5
-        gamma_b = (phi_b.inverse() @ P_b @ H_b.transpose() - pi_ab @ self.phi.transpose() @ self.H.transpose()) @ S_ab **-0.5
-        return r_a, gamma_a, gamma_b, phi_b.transpose() @ H_b.transpose() @ S_ab**-0-5, self.phi.transpose() @ self.H.transpose() @ S_ab **-0.5
+        gamma_a = (pi_ab @ phi_b.transpose() @ H_b.transpose() - np.linalg.inv(self.phi) @ self.P @ self.H.transpose()) @ np.linalg.inv(sqrtm(S_ab))
+        gamma_b = (np.linalg.inv(phi_b) @ P_b @ H_b.transpose() - pi_ab @ self.phi.transpose() @ self.H.transpose()) @ np.linalg.inv(sqrtm(S_ab))
+        return r_a, gamma_a, gamma_b, phi_b.transpose() @ H_b.transpose() @ np.linalg.inv(sqrtm(S_ab)), self.phi.transpose() @ self.H.transpose() @ np.linalg.inv(sqrtm(S_ab))
 
-    #def update(self, r_a, gamma_a, gamma_b, W1, W2, a_id, b_id):
+    def update(self, r_a, gamma_a, gamma_b, W1, W2, id_a, id_b):
+        pi_a = None
+        pi_b = None
+        if(id_a != 1 and id_b != 1):
+            if(id_b == 2):
+                pi_b = self.pi12
+                pi_a = self.pi13
+            if(id_b == 3):
+                pi_b = self.pi13
+                pi_a = self.pi12
+
+        if(id_a != 2 and id_b != 2):
+            if(id_b == 1):
+                pi_b = self.pi12
+                pi_a = self.pi23
+            if(id_b == 3):
+                pi_b = self.pi23
+                pi_a = self.pi12
+
+        if(id_a != 3 and id_b != 3):
+            if(id_b == 1):
+                pi_b = self.pi13
+                pi_a = self.pi23
+            if(id_b == 2):
+                pi_b = self.pi23
+                pi_a = self.pi13
+        if(pi_a is None or pi_b is None):
+            print("pi_a or pi_b not assigned")
+            return
+        gamma = pi_b @ W1 - pi_a @ W2
+        self.state = self.state + self.phi @ gamma @ r_a
+        self.P = self.P - self.phi @ gamma @ gamma.transpose() @ self.phi.transpose()
+        # update pi jl
 
 
 if __name__ == "__main__":
