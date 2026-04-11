786353a6a4424621034f05c2a94c2675990505cd
Fobi-02 <federico@Portatile-Federico.localdomain>
2026-04-11 19:26:23 +0200

    moved parameters to conf limo

--- python_sim/conf_limo.py
+++ python_sim/conf_limo.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 
+# ----------------- LIMO --------------------
 # limo wheel radius
 r = 0.033
 # limo baseline (175mm?)
@@ -21,13 +22,31 @@ v_min = -1
 # limo max yaw rate 2*v_max/(b/2)
 w_max = 0.1*4*v_max/b
 w_min = -0.1*4*v_max/b
+
+# ----------------- SENSORS -------------------
 # lidar covariance
 R = np.identity(3) * 0.01
 # model covariance
 Q = np.identity(3) * 0.001
+
+# ---------------- CONTROLS -------------------
 # distance between target and center
 dist = 1.5
 # raidus of the circle around the central position
 r_circle = 0.5
 # simulation steps for MPC
 N_sim = 100
+
+# ------------- CAMERA ------------------------
+f = 50 # focal length (mm)
+rx = 1920 #resolution of the camera along x
+d = 36/rx # physical size of a pixel (mm)
+n_d = 255 # number of depth channels
+intrinsic_camera = np.array(((1.626e+03, 0, 9.351e+02),(0,1.612e+03, 5.145e+02),(0,0,1)))
+distortion = np.array((0.14321164, -0.37941193, -0.00400418, -0.00202883, -0.25072842))
+
+# ------------ ARUCO -------------------------
+L = 0.06 # half of limo width (along y)
+H = 0.12 # distance from back aruco to center of the limo (along x)
+h = 0.02 # distance from side arucos to center of the limo (along x)
+aruco_size = 0.08 # (m)S
