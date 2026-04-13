137882c46c92a7fff60f233ad496af86ad41a157
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-04-13 12:30:14 +0200

    Updating EKF class

--- python_sim/EKF.py
+++ python_sim/EKF.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import sqrtm
 
 #  ______ _  ________        _               
 # |  ____| |/ /  ____|      | |              
@@ -35,15 +36,17 @@ class EKF:
         self.dt = dt
         self.A = self._A()
         self.G = self._G()
-        self.H = self._H()
+        self.Ha = np.eye(3, 3)
+        self.Hb = np.eye(3, 3)
         self.P = np.linalg.inv(self.H.T @ np.linalg.inv(self.R) @ self.H)
         self.phi = np.eye(3, 3)
-        self.pi12 = np.zeros((3, 3))
-        self.pi13 = np.zeros((3, 3))
-        self.pi14 = np.zeros((3, 3))
-        self.pi23 = np.zeros((3, 3))
-        self.pi24 = np.zeros((3, 3))
-        self.pi34 = np.zeros((3, 3))
+        self.cross_cov = {}
+        self.cross_cov[(1, 2)] = np.zeros((3, 3))
+        self.cross_cov[(1, 3)] = np.zeros((3, 3))
+        self.cross_cov[(1, 4)] = np.zeros((3, 3))
+        self.cross_cov[(2, 3)] = np.zeros((3, 3))
+        self.cross_cov[(2, 4)] = np.zeros((3, 3))
+        self.cross_cov[(3, 4)] = np.zeros((3, 3))
         self.gamma = np.zeros((3,2))
         self.agent_id = type(self).agent_id
         type(self).agent_id += 1
@@ -59,6 +62,7 @@ class EKF:
         x = self.state[0] + self.dt * self.v * np.cos(self.state[2] + self.dt * self.yaw_rate / 2)
         y = self.state[1] + self.dt * self.v * np.sin(self.state[2] + self.dt * self.yaw_rate / 2)
         theta = self.state[2] + self.dt * self.yaw_rate
+
         return np.array([x, y, theta])
     
     def _A(
@@ -68,6 +72,7 @@ class EKF:
         A = np.identity(3)
         A[0, 2] = - self.dt * self.v * np.sin(self.state[2] + self.dt * self.yaw_rate / 2)
         A[1, 2] = self.dt * self.v * np.cos(self.state[2] + self.dt * self.yaw_rate / 2)
+
         return A
     
     def _G(
@@ -80,13 +85,45 @@ class EKF:
         G[1, 0] = self.dt * np.sin(self.state[2] + self.dt * self.yaw_rate / 2)
         G[1, 1] = self.dt ** 2 * self.v / 2 * np.cos(self.state[2] + self.dt * self.yaw_rate / 2)
         G[2, 1] = self.dt
+
         return G
     
-    def _H(
-            self
-            ) -> np.ndarray:
+    def _Ha(
+            self,
+            b_state: np.ndarray
+            ):
         
-        return np.identity(3)
+        Ha = np.zeros((3, 3))
+        Ha[0, 0] = np.cos(self.state[2])
+        Ha[0, 1] = np.sin(self.state[2])
+        Ha[0, 2] = (self.state[1] - b_state[1]) * np.cos(self.state[2]) + (b_state[0] - self.state[0]) * np.sin(self.state[2])
+        Ha[1, 0] = - np.sin(self.state[2])
+        Ha[1, 1] = np.cos(self.state[2])
+        Ha[1, 2] = (b_state[0] - self.state[0]) * np.cos(self.state[2]) + (b_state[1] - self.state[1]) * np.sin(self.state[2])
+        Ha[2, 2] = 1
+        self.Ha = Ha
+    
+    def _Hb(
+            self,
+            ):
+    
+        Hb = np.zeros((3, 3))
+        Hb[0, 0] = np.cos(self.state[2])
+        Hb[0, 1] = np.sin(self.state[2])
+        Hb[1, 0] = - np.sin(self.state[2])
+        Hb[1, 1] = np.cos(self.state[2])
+        Hb[2, 2] = 1
+        self.Hb = Hb
+
+    def h(
+            self,
+            b_state: np.ndarray
+            ) -> np.ndarray:
+        delta_x = (b_state[0] - self.state[0]) * np.cos(self.state[2]) + (b_state[1] - self.state[1]) * np.sin(self.state[2])
+        delta_y = (b_state[1] - self.state[1]) + np.cos(self.state[2]) + (self.state[0] - b_state[0]) * np.sin(self.state[2])
+        delta_theta = b_state[2] - self.state[2]
+
+        return np.array([delta_x, delta_y, delta_theta])
     
     def prediction_step(
             self, 
@@ -100,72 +137,47 @@ class EKF:
         self.P = self.A @ self.P @ self.A.T + self.G @ self.Q @ self.G.T
         self.phi = self.A @ self.phi
 
-    def measurement():
-        return
+    def measurement(
+            self,
+            b_state: np.ndarray,
+            phi_b: np.ndarray,
+            Pb: np.ndarray,
+            z_ab: np.ndarray,
+            id_b: int
+            ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        
+        self._Ha(b_state)
+        self._Hb()
+        piab = self.cross_cov[(min(self.agent_id, id_b), max(self.agent_id, id_b))]
 
-    def update_step(self, lidar_meas):
-        lidar_meas[0] += self.dx
-        lidar_meas[1] += self.dy
-        lidar_meas[2] += self.dtheta
-        S = self.H @ self.P @ self.H.T + self.R
-        W = self.P @ self.H.T @ np.linalg.inv(S) 
-        self.state += W @ (lidar_meas - self.state)
-        self.P = (np.identity(3) - W @ self.H) @ self.P 
+        if self.agent_id > id_b: 
+            piab = piab.T
+        
+        Pab = self.phi @ piab @ phi_b.T
+        Pba = phi_b @ piab.T @ self.phi.T
+        r_a = z_ab - self.h(b_state)
+        S_ab = self.R + self.Ha @ self.P @ self.Ha.T + self.Hb @ Pb @ self.Hb.T - self.Ha @ Pab @ self.Hb.T - self.Hb @ Pba @ self.Ha.T
+        inv_sqrt_S = sqrtm(np.linalg.inv(S_ab))
+        gamma_a = (piab @ phi_b.T @ self.Hb.T - np.linalg.inv(self.phi) @ self.P @ self.Ha.T) @ inv_sqrt_S
+        gamma_b = (np.linalg.inv(phi_b) @ Pb @ self.Hb.T - piab.T @ self.phi.T @ self.Ha.T) @ inv_sqrt_S
+        W1 = phi_b.T @ self.Hb.T @ inv_sqrt_S
+        W2 = self.phi.T @ self.Ha.T @ inv_sqrt_S
 
-    
+        return inv_sqrt_S @ r_a, gamma_a, gamma_b, W1, W2
 
-
-
-
-#  _______        _                     _       
-# |__   __|      | |                   (_)      
-#    | | ___  ___| |_   _ __ ___   __ _ _ _ __  
-#    | |/ _ \/ __| __| | '_ ` _ \ / _` | | '_ \ 
-#    | |  __/\__ \ |_  | | | | | | (_| | | | | |
-#    |_|\___||___/\__| |_| |_| |_|\__,_|_|_| |_|
-                                               
-                                               
-
-import conf_limo
-
-if __name__ == "__main__":
-
-    # initial state
-    initial_state = np.array([0.0, 0.0, 0.0])
-
-    # covariance matricies
-    R = conf_limo.R
-    Q = conf_limo.Q
-
-    # parametri robot
-    r = conf_limo.r
-    b = conf_limo.b
-
-    # crea filtro
-    ekf = EKF(
-        enc_weight=0.5,
-        imu_weight=0.5,
-        r=r,
-        b=b,
-        initial_state=initial_state,
-        R=R,
-        Q=Q,
-        dt=0.1
-    )
-
-    # simple sim
-    for i in range(10):
-
-        w_enc_r = 1.0
-        w_enc_l = 1.0
-        w_imu = 0.1
-
-        ekf.prediction_step(w_enc_r, w_enc_l, w_imu)
-
-        # fake meas
-        lidar_meas = ekf.state + np.random.normal(0, 0.01, 3)
-
-        ekf.update_step(lidar_meas)
-
-        print("state:", ekf.state)
-        print("P matrix: \n", ekf.P)
+    def update_step(
+            self,
+            r_a: np.ndarray,
+            gamma_a: np.ndarray,
+            gamma_b: np.ndarray,
+            W1: np.ndarray,
+            W2: np.ndarray,
+            id_a: int,
+            id_b: int
+            ):
+        
+        self.state = self.state + self.phi @ self.gamma @ r_a
+        self.P = self.P - self.phi @ self.gamma @ self.gamma.T @ self.phi.T  
+        #self.pi12 = self.pi12 - gamma1 @ gamma2.T
+        #self.pi23 = self.pi23 - gamma2 @ gamma3.T
+        #self.pi31 = self.pi31 - gamma3 @ gamma1.T
