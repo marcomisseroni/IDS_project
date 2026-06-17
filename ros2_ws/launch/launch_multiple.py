3e4f548b7b58b3628000acba790259c6f65b47b2
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-06-17 21:14:03 +0200

    fix: add some check on the type of ekf state

--- ros2_ws/launch/launch_multiple.py
+++ ros2_ws/launch/launch_multiple.py
@@ -5,63 +5,63 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='localization',
-            namespace='limo_0',
+            namespace='/limo_0',
             executable='EKF_node',
             name='ekf_node',
             arguments=['0']
         ),
         Node(
             package='localization',
-            namespace='limo_1',
+            namespace='/limo_1',
             executable='EKF_node',
             name='ekf_node',
             arguments=['1']
         ),
         Node(
             package='localization',
-            namespace='limo_2',
+            namespace='/limo_2',
             executable='EKF_node',
             name='ekf_node',
             arguments=['2']
         ),
         Node(
             package='vision',
-            namespace='limo_0',
+            namespace='/limo_0',
             executable='vision_node',
             name='vision_node',
             arguments=['0']
         ),
         Node(
             package='vision',
-            namespace='limo_1',
+            namespace='/limo_1',
             executable='vision_node',
             name='vision_node',
             arguments=['1']
         ),
         Node(
             package='vision',
-            namespace='limo_2',
+            namespace='/limo_2',
             executable='vision_node',
             name='vision_node',
             arguments=['2']
         ),
         Node(
             package='limo_control',
-            namespace='limo_0',
+            namespace='/limo_0',
             executable='MPC_node',
             name='MPC_node',
             arguments=['0']
         ),
         Node(
             package='limo_control',
-            namespace='limo_1',
+            namespace='/limo_1',
             executable='MPC_node',
             name='MPC_node',
             arguments=['1']
         ),
         Node(
             package='limo_control',
-            namespace='limo_2',
+            namespace='/limo_2',
             executable='MPC_node',
             name='MPC_node',
             arguments=['2']
