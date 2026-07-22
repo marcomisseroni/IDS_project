8154cd3ca4257a99f72a9670b615d6ab17221fd0
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-07-22 11:25:16 +0200

    feat: add shell script to run launch file and measurement router

---
+++ ekf_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+cd "$DIR/ros2_ws"
+
+source install/setup.bash
+
+trap 'kill $(jobs -p) 2>/dev/null; sleep 1; kill -9 $(jobs -p) 2>/dev/null; exit 1' SIGINT
+
+ros2 launch launch/launch_multiple.py &
+ros2 run limo_control measurement_router &
+
+wait
