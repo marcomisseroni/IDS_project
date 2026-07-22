91f4243b81326c6974503ffc68a28f3eb2189699
marcomisseroni <marco.misseroni@studenti.unitn.it>
2026-07-22 12:48:22 +0200

    fix: kill all the child processes in ekf_test.sh

--- ekf_test.sh
+++ ekf_test.sh
@@ -6,7 +6,7 @@ cd "$DIR/ros2_ws"
 
 source install/setup.bash
 
-trap 'kill $(jobs -p) 2>/dev/null; sleep 1; kill -9 $(jobs -p) 2>/dev/null; exit 1' SIGINT
+trap 'kill -TERM -- -$$ 2>/dev/null; sleep 1; kill -9 -- -$$ 2>/dev/null; exit 1' SIGINT
 
 ros2 launch launch/launch_multiple.py &
 ros2 run limo_control measurement_router &
